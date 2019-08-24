b4ca1610744b6f56112ffba1a42799bc61922b69
John Azzaro <jazzaro@temple.edu>
2019-08-24 16:23:23 -0700

    functionality for test noted

--- fizzBuzzer.js
+++ fizzBuzzer.js
@@ -2,16 +2,16 @@ module.exports = function(num) {
 	if (typeof num !== 'number') {
 		throw Error('`num` must be a number');
 	}
-	if (num % 15 === 0) {
+	if (num % 15 === 0) {                              // for multiples of 15, return fizzbuzz
 		return 'fizz-buzz';
 	}
-	if (num % 5 === 0) {
+	if (num % 5 === 0) {                               // for multiples of 5, return buzz
 		return 'buzz';
 	}
-	if (num % 3 === 0) {
+	if (num % 3 === 0) {                               // for multiples of 3, return fizz
 		return 'fizz';
 	}
-	else {
+	else {                                             // for everything else, return the number.
 		return num;
 	}
 }
